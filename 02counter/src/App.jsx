34d5e8de84855ca30234e03213b016cd191bd9ae
Mayank Nandwani <nandwanimayank007@gmail.com>
2023-12-08 14:24:28 +0530

    useState

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(5)
+  // let a=5;
+
+  const addValue =()=>{
+    // setCount=count+1;
+    if(count<20)
+    setCount(count+1);
+  }
+  const removeValue =()=>{
+    if(count>0)
+    setCount(count-1);
+  }
+
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>counter vallue: {count}</h2>
+      <button
+      onClick={addValue}>Add value</button>
+      <br />
+      <button
+      onClick={removeValue}>remove value</button>
+
+    </>
+  )
+}
+
+export default App
